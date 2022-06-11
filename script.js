43fe09ecd89c5150a5e8493e574867e5f8b0bbe0
fabbrob <robfabbro50@gmail.com>
2022-06-11 17:03:24 +1000

    added comments for the javascript code

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
+//getters for the tiles and keys in their respective arrays
 const tiles = document.querySelectorAll('.tile');
 const keys = document.querySelectorAll('.letter');
+
+//getter for the backspace key
 const backspaceKey = document.querySelector('#backspace');
 
+//a variable that represents what tile is the next to be typed into
 let currentTileIndex = 0;
 
+//function that allows for the input from clicking on the keyboard letters
 function keyPressed(event) {
 
     const input = event.target.innerText;
@@ -14,6 +18,7 @@ function keyPressed(event) {
     currentTileIndex++;
 }
 
+//function that allows for the backspace key to work accordingly
 function backspace(event) {
     if(tiles[currentTileIndex].innerText === ''){
         tiles[currentTileIndex-1].innerText = '';
@@ -24,8 +29,10 @@ function backspace(event) {
     }
 }
 
+//adds the key pressing functionality for each letter key
 for(const key of keys) {
     key.addEventListener('click', keyPressed);
 }
 
+//adds the backspacing functionality for the backspace key
 backspaceKey.addEventListener('click', backspace);
