ed19f21c2d97e20fb7911e87226221e40d7a6d42
fabbrob <robfabbro50@gmail.com>
2022-06-11 16:58:36 +1000

    added keyboard functionality to the letter and backspace keys

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-const tiles = document.getElementsByClassName('tile');
-const keys = document.getElementsByClassName('key');
+
+const tiles = document.querySelectorAll('.tile');
+const keys = document.querySelectorAll('.letter');
+const backspaceKey = document.querySelector('#backspace');
+
+let currentTileIndex = 0;
+
+function keyPressed(event) {
+
+    const input = event.target.innerText;
+
+    tiles[currentTileIndex].innerText = input;
+
+    currentTileIndex++;
+}
+
+function backspace(event) {
+    if(tiles[currentTileIndex].innerText === ''){
+        tiles[currentTileIndex-1].innerText = '';
+    }
+
+    if(currentTileIndex > 0){
+        currentTileIndex--;
+    }
+}
+
+for(const key of keys) {
+    key.addEventListener('click', keyPressed);
+}
+
+backspaceKey.addEventListener('click', backspace);
